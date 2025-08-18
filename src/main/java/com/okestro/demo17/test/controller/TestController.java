0395636d2214cbcb755c71cacc020a603431b4c9
dariouse <dariouse@gmail.com>
2025-08-18 10:18:03 +0900

    request count 추가

--- src/main/java/com/okestro/demo17/test/controller/TestController.java
+++ src/main/java/com/okestro/demo17/test/controller/TestController.java
@@ -6,19 +6,35 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class TestController {
 
     private final TestService testService;
+    private final ReentrantLock lock = new ReentrantLock();
+    private int totalRequests = 0;
+
+    private int incAndGetInFlight() {
+
+        lock.lock();
+        try {
+            totalRequests++;
+            return totalRequests;
+        } finally {
+            lock.unlock();
+        }
+    }
 
     @GetMapping("/test")
-    public String test() throws Exception {
+    public String test() {
 
-        log.info("Request {}", Thread.currentThread());
+        int now = incAndGetInFlight();
+
+        log.info("Request {} {}", Thread.currentThread(), now);
         var result = testService.Instance();
-        log.info("Response {}", Thread.currentThread());
 
         return "OK";
     }
